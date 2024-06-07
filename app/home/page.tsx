edb32f1e4bb655a3493967c3c967256970de6d28
0xBoom <boom-wlk@hotmail.com>
2024-06-07 11:49:45 +0700

    error can't show images

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+import Game from '@/ui/components/game/Game'
+
+export default function Home(){
+    return(
+        <div>
+            <h1>Home</h1>
+            <Game tab="Casino"/>
+        </div>
+    )
+}
---
+++ ui/components/game/Game.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+
+interface Props {
+  tab: string;
+}
+
+const Game: React.FC<Props> = ({ tab }) => {
+
+  const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const data = [
+    {
+      provider: '',
+      providerName: '',
+      gameType: [],
+      logoURL: '',
+      logoMobileURL: '',
+      logoTransparentURL: '',
+      image: {
+        vertical: '',
+        horizontal: '',
+        banner: ''
+      },
+      status: ''
+    }
+  ]
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://api-test.ambexapi.com/api/v1/ext/gameList/664DF64C2EA2D0B684D1F1F7/testopuplunarr`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            agentUsername: 'testopuplunarr',
+            key: 'OdcwK_23oCJl9IQBLQRZn',
+            tab,
+            web: 'sportbook101',
+          }),
+        });
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        console.log(data);
+        setGames(data);
+        setLoading(false);
+        console.log(games)
+      } catch (error: any) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [tab]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default Game;
