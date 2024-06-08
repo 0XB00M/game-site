eeb76c73890650257d49c948738c49075e56da77
DERIF01 <vego2555@gmail.com>
2024-06-08 13:38:41 +0700

    Add files via upload

---
+++ app/admin-panel/history/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from '@/ui/components/userform/HistoryForm';
+
+const Page: React.FC = () => {
+    return (
+        <div>
+            <Form />
+        </div>
+    );
+};
+
+export default Page;
---
+++ app/admin-panel/menu/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SlideMenu from '../../../ui/components/userform/SlideMenu';
+
+const Page: React.FC = () => {
+    return (
+        <div>
+            <SlideMenu />
+        </div>
+    );
+};
+
+export default Page;
